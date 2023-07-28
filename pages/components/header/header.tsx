fb3be85e3a7607ed5d44b04c5be72fc07b486ff2
douglas <douglas.jansey@gmail.com>
2023-07-28 19:02:05 -0300

    add: add links at the header

--- pages/components/header/header.tsx
+++ pages/components/header/header.tsx
@@ -1,7 +1,30 @@
+/* eslint-disable react/jsx-key */
+import Link from "next/link";
+import style from "../../../styles/HeaderStyle/Header.module.sass";
+
 export default function header() {
+  const menu: Array<string> = [
+    "Receitas",
+    "Minhas Receitas",
+    "Cadastrar Receitas",
+    "Categoria",
+  ];
+
   return (
     <>
-      <p> Header</p>
+      <div className={style.container}>
+        <div><h2><Link href="/">Logo</Link></h2></div>
+        <nav className={style.nav}>
+          {menu.map((item, index) => (
+            <div key={index}>
+              <a className={style.linkStyle} href="#">{item}</a>
+            </div>
+          ))}
+        </nav>
+        <div className={style.profile}>
+          <p>Perfil</p>
+        </div>
+      </div>
     </>
   );
 }
